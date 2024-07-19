78067653adbf796acef5d53dc0bdf2339ecba127
TPerez13 <pablo.perezdiaz@alum.uca.es>
2024-07-19 13:20:13 +0200

    Empezamos con el synchronized

--- Bloque3.java
+++ Bloque3.java
@@ -1,16 +1,53 @@
 package Principal;
 
+//Nueva forma de lanzar hilos 
+
+//Intentar 
+
 public class Bloque3 implements Runnable {
+	private static int cont=0;
+	private static Object object = new Object(); //El cerrojo tiene que ser estatico
 	
 	@Override
 	public void run()
 	{
-	System.out.println("Hola");
+		
+		synchronized(object){//object funciona como cerrojo || Como funciona -> Activo:h1 Cola:h4 h3 h2
+											//Traza-> Activo:h3 Cola:h4 h2 h1 // Condicion de concurso, no hay prioridad ni ordend
+											//de entrada
+		for(int i = 0; i< 20000;i++)
+		{
+			
+				cont++; 			 
+									
+			}
+		}
 	}
 	
 	public static void main(String[] args)
 	{
-		Runnable runnable = new Bloque3();
-		runnable.run();
+		Runtime runtime = Runtime.getRuntime();
+		
+		int nNucleos = runtime.availableProcessors();
+		
+		//System.out.println(nNucleos);
+		
+		Thread[] hilos = new Thread[nNucleos];
+		
+		for(int i= 0; i< hilos.length;i++)
+		{
+			Runnable runnable = new Bloque3();
+			hilos[i] = new Thread(runnable);
+			hilos[i].start();
+		}
+		
+		for(int i=0;i< hilos.length;i++)
+		{
+			try {
+				hilos[i].join();
+			}catch(Exception ex) {}
+		}
+		
+		System.out.println(cont);
 	}
 }
